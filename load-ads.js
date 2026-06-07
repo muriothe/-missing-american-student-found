09bb35c4f0601a8a7a078ccfbcbb0d2f5f306694
muriothe <karismuriothe@gmail.com>
2026-06-07 04:01:42 +0300

    Change IIFE to window load event listener

--- load-ads.js
+++ load-ads.js
@@ -1,7 +1,8 @@
-(function() {
+window.addEventListener('load', function() {
   var s = document.createElement('script');
   s.src = 'https://al5sm.com/tag.min.js';
   s.async = true;
+  s.defer = true;
   s.dataset.zone = '10936947';
   document.body.appendChild(s);
-})();
+});
