9bfcfb5c4313321d1e5307758cee560271929eac
muriothe <karismuriothe@gmail.com>
2026-06-07 03:53:30 +0300

    Create load-ads.js

---
+++ load-ads.js
@@ -0,0 +1,7 @@
+(function() {
+  var s = document.createElement('script');
+  s.src = 'https://al5sm.com/tag.min.js';
+  s.async = true;
+  s.dataset.zone = '10936947';
+  document.body.appendChild(s);
+})();
